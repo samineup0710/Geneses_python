b687296aaa99d3921ac369cd77ee31459b300b23
SAMIDHA <samida226077@gmail.com>
2020-11-04 11:24:03 +0545

    exercise1 question solutions.

---
+++ exercise1.py
@@ -0,0 +1,31 @@
+width = 17
+height = 12.0
+delimiter ='.'
+
+"""OUTPUTS"""
+
+"""1) width/2"""
+wide1 = width/2
+print("value is:", wide1)
+print("the type of this exp is:", type(wide1))
+
+"""2) width/2.0"""
+wide2 = width/2.0
+print("value is:", wide2)
+print("the type of this exp is:", type(wide2))
+
+"""3) height/3"""
+high = height/3
+print("value is:", high)
+print("the type of this exp is:", type(high))
+
+
+"""4) 1 + 2*5 """
+exp = 1 +2* 5
+print("value is:", exp)
+print("the type of this exp is:", type(exp))
+
+"""5) delimeter*5 """
+delm = delimiter*5
+print("value is:", delm)
+print("the type of this exp is:", type(delm))
