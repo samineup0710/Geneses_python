dbb4379e7527bedce7f813ab28b21f3a522c60b9
SAMIDHA <samida226077@gmail.com>
2020-11-04 11:28:33 +0545

    Lists creation and finding element at indexes.

---
+++ exercise4.py
@@ -0,0 +1,10 @@
+list1 = ["hello", "1", 3, 34, "dogs", "person"] 
+
+print(len(list1))
+"""indexing method"""
+print("list element at first is:", list1[0])     #1st element
+print("list element at fourth is:", list1[3])     #4th element   
+
+
+"""print(type(list1[1]))
+print(type(list1[2]))"""
